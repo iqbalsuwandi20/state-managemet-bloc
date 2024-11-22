447ab1bce57c4f416dd2b2c64c9977945039a3e4
iqbalsuwandi20 <iqbalsuwandi20@gmail.com>
2024-11-22 18:09:37 +0700

    basic cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,31 +10,39 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: HomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class CounterCubit extends Cubit<int> {
+  CounterCubit({this.initialData = 0}) : super(initialData);
 
-  Stream<int> countStream() async* {
-    for (var i = 0; i <= 10; i++) {
-      await Future.delayed(const Duration(seconds: 1));
-      yield i;
-    }
+  int initialData;
+
+  void tambahData() {
+    emit(state + 1);
   }
+
+  void kurangData() {
+    emit(state - 1);
+  }
+}
+
+// ignore: must_be_immutable
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
+
+  CounterCubit myCounter = CounterCubit(initialData: 49);
 
   @override
   Widget build(BuildContext context) {
-    // ignore: avoid_print
-    print("REBUILD");
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Stream Apps".toUpperCase(),
+          "Basic Cubit".toUpperCase(),
           style: const TextStyle(
             color: Colors.white,
             fontWeight: FontWeight.bold,
@@ -42,28 +51,61 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.blue[900],
         centerTitle: true,
       ),
-      body: StreamBuilder<int>(
-        stream: countStream(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(
-                color: Colors.blue[900],
-              ),
-            );
-          } else {
-            return Center(
-              child: Text(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          StreamBuilder(
+            initialData: myCounter.initialData,
+            stream: myCounter.stream,
+            builder: (context, snapshot) {
+              return Text(
                 "${snapshot.data}",
+                textAlign: TextAlign.center,
                 style: const TextStyle(
                   color: Colors.black,
                   fontSize: 50,
                   fontWeight: FontWeight.bold,
                 ),
+              );
+            },
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                style:
+                    ElevatedButton.styleFrom(backgroundColor: Colors.blue[900]),
+                onPressed: () {
+                  myCounter.kurangData();
+                },
+                child: const Text(
+                  "-",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
-            );
-          }
-        },
+              ElevatedButton(
+                style:
+                    ElevatedButton.styleFrom(backgroundColor: Colors.blue[900]),
+                onPressed: () {
+                  myCounter.tambahData();
+                },
+                child: const Text(
+                  "+",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
