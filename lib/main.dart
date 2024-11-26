593dd228e9c03a0f06da89518707d0e9e9b23073
iqbalsuwandi20 <iqbalsuwandi20@gmail.com>
2024-11-26 15:15:54 +0700

    bloc selector

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'app.dart';
-import 'controllers/counter.dart';
-import 'controllers/theme.dart';
+import 'controllers/user_bloc.dart';
+import 'pages/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -14,16 +13,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => CounterBloc(),
-        ),
-        BlocProvider(
-          create: (context) => ThemeBloc(),
-        ),
-      ],
-      child: const App(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BlocProvider(
+        create: (context) => UserBloc(),
+        child: HomePage(),
+      ),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,104 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-import '../controllers/counter.dart';
-import '../controllers/theme.dart';
+import 'package:state_management_bloc/controllers/user_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    CounterBloc counterBloc = context.read<CounterBloc>();
-    ThemeBloc themeBloc = context.read<ThemeBloc>();
+    UserBloc userBloc = context.read<UserBloc>();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Multi Bloc Listener".toUpperCase(),
+          "Bloc selector".toUpperCase(),
           style: const TextStyle(
             fontWeight: FontWeight.bold,
           ),
         ),
         centerTitle: true,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: ListView(
+        padding: const EdgeInsets.all(20),
         children: [
-          MultiBlocListener(
-            listeners: [
-              BlocListener<ThemeBloc, bool>(
-                  listener: (context, state) =>
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text(
-                            "Tema gelap aktif".toUpperCase(),
-                          ),
-                          duration: const Duration(seconds: 1),
-                        ),
-                      ),
-                  listenWhen: (previous, current) {
-                    if (current == false) {
-                      return true;
-                    } else {
-                      return false;
-                    }
-                  }),
-              BlocListener<CounterBloc, int>(
-                  listener: (context, state) =>
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text(
-                            "Lebih dari 5".toUpperCase(),
-                          ),
-                          duration: const Duration(seconds: 1),
-                        ),
-                      ),
-                  listenWhen: (previous, current) {
-                    if (current > 5) {
-                      return true;
-                    } else {
-                      return false;
-                    }
-                  }),
-            ],
-            child: BlocBuilder<CounterBloc, int>(
-              bloc: counterBloc,
-              builder: (context, state) => Text(
-                '$state',
-                style: const TextStyle(
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+          BlocSelector<UserBloc, Map<String, dynamic>, String>(
+            selector: (state) => state['name'],
+            builder: (context, state) => Text(
+              "Nama: $state".toUpperCase(),
+            ),
+          ),
+          BlocSelector<UserBloc, Map<String, dynamic>, int>(
+            selector: (state) => state['age'],
+            builder: (context, state) => Text(
+              "Umur: $state Tahun".toUpperCase(),
+            ),
+          ),
+          const SizedBox(height: 20),
+          TextField(
+            onChanged: (value) => userBloc.changeName(value),
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            "Berapa umur anda:".toUpperCase(),
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
             ),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               IconButton(
-                onPressed: () => counterBloc.remove(),
-                icon: const Icon(
-                  Icons.remove,
-                  size: 50,
-                ),
-              ),
+                  onPressed: () =>
+                      userBloc.changeAge(userBloc.state['age'] - 1),
+                  icon: const Icon(
+                    Icons.remove,
+                    size: 50,
+                  )),
               IconButton(
-                onPressed: () => counterBloc.add(),
-                icon: const Icon(
-                  Icons.add,
-                  size: 50,
-                ),
-              ),
+                  onPressed: () =>
+                      userBloc.changeAge(userBloc.state['age'] + 1),
+                  icon: const Icon(
+                    Icons.add,
+                    size: 50,
+                  )),
             ],
           ),
         ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => themeBloc.changeTheme(),
-        child: const Icon(
-          Icons.theater_comedy_outlined,
-        ),
       ),
     );
   }
